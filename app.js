e2d49708ea274113d1c41102a09f5603fdef9cae
Adelkilde <William.adelkilde@gmail.com>
2023-09-20 14:06:02 +0200

    🌟

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(express.json()); // to parse JSON bodies
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send("Node Express REST Users API 🌟");
+  res.send("Node Express REST Users API");
 });
 
 // READ all users
